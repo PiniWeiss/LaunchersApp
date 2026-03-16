88e4e2b053b2eeb357c58518b210f1d5e742749a
pini weiss <pini6775@gmail.com>
2026-03-16 11:56:36 +0200

    create launcher endpoint created

--- server/controllers/launcher.controller.js
+++ server/controllers/launcher.controller.js
@@ -1,13 +1,33 @@
 import Launcher from "../models/db.model.js";
 
-export const getLaunchers = async(req, res) => {
+export const getLaunchers = async (req, res) => {
     try {
         const launchers = await Launcher.find()
-        return res.status(200).json({message:'get user info succrssfully', data: launchers})
+        return res.status(200).json({ message: 'get Launchers succrssfully', data: launchers })
 
     } catch (error) {
-        console.log('Error in get self info controller', error.message);
-        return res.status(500).json({message: 'Internal server error '});
+        console.log('Error in getLauncher controller', error.message);
+        return res.status(500).json({ message: 'Internal server error ' });
+    }
+}
+
+export const createLauncher = async (req, res) => {
+    try {
+        const { name, city, rocketType, latitude, longitude } = req.body
+        if (!name, !city, !rocketType, !latitude, !longitude) res.status(401).json({ message: "One of the feilds is missing" })
+        const newLauncher = await new Launcher({
+            name,
+            city,
+            rocketType,
+            latitude,
+            longitude
+        })
+        newLauncher.save()
+        return res.status(200).json({ message: 'get user info succrssfully', data: newLaunch })
+    } catch (error) {
+        console.log('Error in createLauncher controller', error.message);
+        return res.status(500).json({ message: 'Internal server error ' });
+
     }
 }
 
--- server/routes/launcher.route.js
+++ server/routes/launcher.route.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { getLaunchers } from '../controllers/launcher.controller.js';
+import { createLauncher, getLaunchers } from '../controllers/launcher.controller.js';
 
 const router = express.Router();
 
 router.get('/', getLaunchers);
-// router.post('/', 'controller Function');
+router.post('/', createLauncher);
 // router.put('/', 'controller Function');
 // router.delete('/', 'controller Function');
 
